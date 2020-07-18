6f606ce3d866e760e83de5e705670882bd1cfa6d
alvind1 <alvin.dai.02@gmail.com>
2020-07-18 12:02:53 -0400

    update

--- generateData.js
+++ generateData.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const uuid = require('uuid');
+const utils = require('./utils');
+const print = utils.print;
 
+/*let data = {}
+for(i=0; i<20; i++){
+    let student = { 
+        name: 'Mike',
+        age: 23, 
+        gender: 'Male',
+        department: 'English',
+        car: 'Honda' 
+    };
+    data[JSON.stringify(uuid.v4())] = 2;
+}*/
+
+let dict = {}
+
+for(i=0; i<20; i++){
+    let student = { 
+        seller: 'Mike',
+        price: 23, 
+        gender: 'Male',
+        description: 'English',
+        
+        car: 'Honda' 
+    };
+    dict[uuid.v4()] = student;    
+}
+ 
+let data = JSON.stringify(dict);
+fs.writeFileSync('postingData.json', data);
---
+++ utils.js
@@ -0,0 +1,8 @@
+const print = (x)=>{
+    console.log(x);
+}
+
+
+module.exports = {
+    print : print
+}
